fc11fe7a766ff89a1e3be24e4ebe3eb745610be9
Franklin Junior <franklinjunior12@hotmail.com>
2025-05-26 11:40:11 -0300

    new platform spawning

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,6 +1,6 @@
 extends Camera2D
 
-@export var CAMERA_SCROLL_SPEED = 0.5
+@export var CAMERA_SCROLL_SPEED = 0.15
 
 func _process(delta: float) -> void:
 	position.y -= CAMERA_SCROLL_SPEED
--- scripts/world.gd
+++ scripts/world.gd
@@ -7,6 +7,13 @@ var player = null
 var scoreLabel = null
 var camera = null
 var view_y = 0
+var last_platform_num = 1
+
+const platform = preload("res://scenes/platform.tscn")
+const platform_str = "Platform"
+const platform_spawn_dist = 100
+const platform_x_spawn_dist = 100
+const platform_y_spawn_dist = 100
 
 func _ready() -> void:
 	player = get_node("Player")
@@ -18,10 +25,25 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	var new_y = player.position.y
+	var view_y_end = view_y + camera.position.y
+	if new_y > view_y_end:
+		print("game over")
 	if new_y < current_player_y:
 		current_player_y = new_y
 		score = -roundi(current_player_y - initial_player_y)
 		scoreLabel.text = "Score " + str(score)
-	var view_y_end = view_y + camera.position.y
-	if new_y > view_y_end:
-		print("game over")
+	var last_platform = get_node(last_platform_name())
+	if last_platform.position.distance_to(player.position) <= platform_spawn_dist:
+		var new_platform = platform.instantiate()
+		last_platform_num += 1
+		new_platform.name = last_platform_name()
+		var signal_mult = 1
+		if randi() % 2 == 0:
+			signal_mult = -1
+		new_platform.position.x = last_platform.position.x + signal_mult * platform_x_spawn_dist
+		signal_mult = -1
+		new_platform.position.y = last_platform.position.y + signal_mult * platform_y_spawn_dist
+		add_child(new_platform)
+
+func last_platform_name() -> String:
+	return platform_str + str(last_platform_num)
